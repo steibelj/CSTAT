fb80890f9e08df808af82988d81a4eb9b9683776
Juan Steibel <steibelj@msu.edu>
2021-03-11 09:51:18 -0500

    Add files via upload

--- factors/Factor.R
+++ factors/Factor.R
@@ -1,13 +1,15 @@
 #fitting linear classification effects models
 #learning objective 1: Understand coding of classification effects linear models, alternative parametrizations
-#learning objective 2: Understand modeling of more than one factor (RCBM) and "random" vs "fixed effects"
+#learning objective 2: Understand modeling of more than one factor (RCBD) and "random" vs "fixed effects"
 #NOTE: When we treat a factor as random, is the first multi-level model we use in class
 #learning objective 3: Modify code and compare effect of prior
+library(lme4)
+library(lmerTest)
 
 library(rstan)
 library(shinystan)
 library(bayesplot)
-library(lme4)
+
 rm(list=ls())
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
@@ -40,19 +42,24 @@ monitor(stan_anova)
 traceplot(stan_anova)
 summary(stan_anova,pars=c('S1',"S2","diff","vare"))$summary
 
-
+mcmc_acf(stan_anova,pars=c('S1',"S2","diff","vare"))
 
 mcmc_intervals(stan_anova,pars=c("diff","vare"),prob = 0.95,prob_outer = 0.99)
 
 plot_title<-ggtitle("Posterior distributions","difference and mean by sex")
 mcmc_areas(stan_anova,pars = c("diff","S1","S2"),prob=0.95,prob_outer = 0.99)+plot_title
 
+mcmc_scatter(stan_anova,pars = c("S1","S2"))+
+  geom_abline(slope = 1,intercept = 0,lwd=2,color="red")
+
 #cell mean model
 stan_cell_m<-stan(file="cell_mean.stan",data=data_sex,warmup = 1000,thin = 1, chains = 4, iter = 2000)
 #save(stan_cell_m,file="cell_mean.RData")
 
 monitor(stan_cell_m)
+monitor(stan_anova)
 traceplot(stan_cell_m)
+mcmc_trace(stan_cell_m)
 summary(stan_cell_m,pars=c("sexeff","diff","vare"))$summary
 
 plot_title<-ggtitle("Posterior distributions","difference and mean by sex")
@@ -74,14 +81,16 @@ data_matings
 lmo<-lm(Y~as.factor(sex)+as.factor(mating),data=data_matings)
 summary(lmo)
 lsmeans::lsmeans(lmo,"sex")
-anova(lmo)
 
-stan_fix0<-stan(file="fixed_matings.stan",data=data_matings,warmup = 2000,thin = 5, chains = 4, iter = 6000)
+stan_fix0<-stan(file="fixed_matings.stan",data=data_matings,warmup = 500,thin = 1, chains = 4, iter = 1500)
+
 #save(stan_fix0,file="fixed_mating.RData")
 #check for warnings....
-pairs(stan_fix0,pars=c("mateff","diff","S1","S2","vare"),include=F)
+#pairs(stan_fix0,pars=c("mateff","diff","S1","S2","vare"),include=F)
 
 monitor(stan_fix0)
+mcmc_acf(stan_fix0,pars=c("diff","S1","S2","vare"))
+
 sm0<-summary(stan_fix0,pars=c("S1","S2","diff","vare"))
 sm0$summary
 
@@ -90,23 +99,16 @@ mcmc_areas(stan_fix0,pars = c("diff","vare"),prob=0.95,prob_outer = 0.99)+plot_t
 
 
 
-#an idea to deal with the warning
-stan_fix<-stan(file="fixed_matings.stan",data=data_matings,warmup = 2000,thin = 5, chains = 4, iter = 6000,
-               control = list(max_treedepth = 15))
-monitor(stan_fix)
-traceplot(stan_fix,pars=c("sexeff","diff","S1","S2"))
-pairs(stan_fix,pars=c("mateff","diff","vare"),include=F)
-
-
-summary(stan_fix)$summary
 
 
 lmr<-lmer(Y~as.factor(sex)+(1|mating),data=data_matings)
 summary(lmr)
 lsmeans::lsmeans(lmr,"sex") 
+anova(lmr,ddf="Satterthwaite")
 
 
-stan_ran<-stan(file="random_matings.stan",pars=c("diff","vare","varm","sexeff"),data=data_matings,warmup = 3000,thin = 5, chains = 4, iter = 10000)
+stan_ran<-stan(file="random_matings.stan",pars=c("diff","vare","varm","sexeff"),data=data_matings,warmup = 2000,thin = 5, chains = 4, iter = 7000,
+               control = list(adapt_delta = 0.99))
 #save(stan_ran,file="ran_mating.RData")
 pairs(stan_ran)
 
